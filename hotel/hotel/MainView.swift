82db4c47adc8dad112528308d40be4b06c17d158
ChebaturoVN <kukuruza2488@gmail.com>
2023-09-08 12:24:50 +0300

    create_project
    create_project_xcode

---
+++ hotel/hotel/MainView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  hotel
+//
+//  Created by VladimirCH on 08.09.2023.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
---
+++ hotel/hotel/hotelApp.swift
@@ -0,0 +1,17 @@
+//
+//  hotelApp.swift
+//  hotel
+//
+//  Created by VladimirCH on 08.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct hotelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
